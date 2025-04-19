d10f501997ce3351716b41c96068a4bb6060b865
Mena Song-Lew <msonglew@gmail.com>
2025-04-18 20:45:59 -0700

    Add global.js

---
+++ global.js
@@ -0,0 +1,80 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+};
+
+// NAV BAR
+
+let pages = [
+    { url: '', title: 'Home'},
+    { url: 'contact/', title:'Contact'},
+    { url: 'projects/', title:'Projects'},
+    { url: 'resume/', title:'Resume'},
+    { url: 'https://github.com/msonglew', title:'GitHub'},
+]
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+? "/"                  // Local server
+: "/website/";         // GitHub Pages repo name
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    if (!url.startsWith('http')) {
+        url = BASE_PATH + url;
+    };
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    };
+
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    };
+
+};
+
+// THEME MODE DROP DOWN
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme: 
+          <select>
+              <option value="light dark">Automatic</option>
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+          </select>
+      </label>`,
+);
+
+const select = document.querySelector(".color-scheme select");
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+
+    localStorage.colorScheme = event.target.value;
+});
+
+if (localStorage.colorScheme) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+}
+
+
+
+
+
+
+
